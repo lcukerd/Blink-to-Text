b1e88d127789405814ccc4ee42f214a61c6f94b5
lcukerd <lcukerd@github.com>
2018-09-25 21:57:19 +0530

    UI done

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain
+} = electron;
+
+process.env.NODE_ENV = 'development';
+
+let mainWindow;
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 870,
+    frame: false
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname : path.join(__dirname,'../html/mainWindow.html'),
+    protocol : 'file',
+    slashes : true
+  }));
+
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+});
---
+++ js/mainWindow.js
@@ -0,0 +1,4 @@
+const electron = require('electron');
+const {ipcRenderer} = electron;
+const ul = document.querySelector('ul');
+const head = document.querySelector('#heading');
